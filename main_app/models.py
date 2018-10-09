fc5be50b5246a54bae3b24d3968763c4410d9658
Devan Grose <devangrose@gmail.com>
2018-10-09 14:14:57 -0700

    added date info to order history

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
+import datetime
 
 
 
@@ -15,7 +16,7 @@ class Product(models.Model):
         return self.name
 
 class Order(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     quantity = models.IntegerField()
 
@@ -25,6 +26,7 @@ class Cart(models.Model):
     orders = models.ManyToManyField(Order, related_name='CartOrders')
     user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     closed = models.BooleanField(default = False)
+    date = models.DateField(default=datetime.datetime.now)
 
     def __str__(self):
         return str(self.user_id)
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+import datetime
 
 # Create your views here.
 def index (request):
