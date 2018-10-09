4e3a55b7ad46e4c782cb5a3ea44772736158ceda
Devan Grose <devangrose@gmail.com>
2018-10-09 09:30:26 -0700

    changed models to reference user

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from django.contrib.auth.models import User
+
 
 
 # Create your models here.
@@ -7,10 +9,20 @@ class Product(models.Model):
     image_url = models.CharField(max_length=255)
     name = models.CharField(max_length=255)
     description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.DO_NOTHING)
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     quantity = models.IntegerField()
+
+    def __str__(self):
+        return str(product) + ', ' + str(quantity)
 class Cart(models.Model):
     orders = models.ManyToManyField(Order, related_name='CartOrders')
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+
+    def __str__(self):
+        return str(user_id)
